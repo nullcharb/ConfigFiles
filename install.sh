453f51b1144d0b461be4cd06c9a103e4b18744f5
nu11charb@gmail.com <nu11charb@gmail.com>
2021-04-04 16:46:15 +1000

    Fixed the install.sh file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt-get install tmux curl git zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp ./gdbinit ~/gdbinit
+cp ./gdbinit ~/.gdbinit
 cp ./tmux.conf ~/.tmux.conf
 cp ./zshrc ~/.zshrc
